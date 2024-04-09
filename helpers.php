b6d382972d91ff08c11b907e2616951007c2b99e
Nathaniel-ray <raymondanyanwu7@gmail.com>
2024-04-09 11:25:22 +0100

    initial commit

---
+++ helpers.php
@@ -0,0 +1,13 @@
+<!-- this has helper functions that will be used all over the file -->
+<?php
+
+/**
+ * get the base path  of the project.
+ * 
+ * @param string path
+ * @return string
+ */
+function basePath($path = '')
+{
+    return __DIR__ . '/' . $path;
+}
